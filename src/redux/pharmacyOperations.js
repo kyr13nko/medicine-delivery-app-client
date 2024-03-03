0d13a718ee337814912b8d64e561e021f3da34d5
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2024-03-03 19:19:32 +0200

    changed host

--- src/redux/pharmacyOperations.js
+++ src/redux/pharmacyOperations.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'http://localhost:8080/api';
-// axios.defaults.baseURL = 'https://medicine-delivery-app-server.onrender.com/api';
+// axios.defaults.baseURL = 'http://localhost:8080/api';
+axios.defaults.baseURL = 'https://medicine-delivery-app-server.onrender.com/api';
 
 export const fetchPharmacy = createAsyncThunk(
   'pharmacies/fetchAll',
