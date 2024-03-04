b04331666a2195e080c733a4a4bcc5d0927b1d50
Hryhorii Kyriienko <hrhrkrnk@gmail.com>
2024-03-04 05:05:51 +0200

    loader fix

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -5,6 +5,11 @@ const Loader = () => {
   return (
     <Wrapper>
       <FallingLines color="#008080" width="100" visible={true} ariaLabel="falling-lines-loading" />
+      <p>
+        Our application database is hosted on the free render.com service. <br />
+        Therefore, you need to wait until all the data is loaded. <br />
+        Thank you for waiting!
+      </p>
     </Wrapper>
   );
 };
